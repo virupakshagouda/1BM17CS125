98513fc6acefaedc686ad9239770ae155b90c9d0
virupakshagouda <52944329+virupakshagouda@users.noreply.github.com>
2019-09-12 12:55:16 +0530

    Update lab4_125.py

--- lab4_125.py
+++ lab4_125.py
@@ -1,8 +1,8 @@
 class student:
     def __init__(self):
-        self.sid=None
-        self.age=None
-        self.marks=None
+        self.__sid=None
+        self.__age=None
+        self.__marks=None
     def validate_marks(self):
         if self.marks>=0 and self.marks<=100:
             return True
